fc71ae1486028955311c23c33e89a952922d121a
Peter Geyser <peterpgeyser@gmail.com>
2019-09-16 18:11:09 -0700

    Starting router module functionality

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 //dependencies
 var express = require("express");
 var path = require("path");
+var HTMLROUTER = require("./app/routing/htmlRoutes.js");
+var APIROUTER = require("./app/routing/apiRoutes.js");
 
 //server configuration
 var app = express();
@@ -11,3 +13,13 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.json());
+
+//new HTMLROUTER object from constructor in htmlRoutes.js
+var htmlRouter = new HTMLROUTER();
+htmlRouter.routeHTML();
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
