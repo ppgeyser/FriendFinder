1c2a0d6de84d828fb2760975036da2d3c4559acf
Peter Geyser <peterpgeyser@gmail.com>
2019-09-18 15:12:49 -0700

    html routing functionality complete

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,29 +1,17 @@
 //dependencies
-var express = require("express");
 var path = require("path");
 
-//server configuration
-var app = express();
-// var PORT = 3000;
-var PORT = process.env.PORT || 3000;
-
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(express.json());
-
 //HTMLROUTER constructor to be called in server.js
-var HTMLROUTER = function() {
-    this.routeHTML = function () {
-        
-        app.get("/", function(req, res) {
-            res.sendFile(path.join(__dirname, "../public/home.html"));
-          });
-          
-          app.get("/survey", function(req, res) {
-            res.sendFile(path.join(__dirname, "../public/survey.html"));
-          });
-    }
-};
+module.exports = function (app) {
+  
+  //route to survey page
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
 
-module.exports = HTMLROUTER;
+  //default route
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+}
--- server.js
+++ server.js
@@ -1,25 +1,20 @@
 //dependencies
 var express = require("express");
-var path = require("path");
-var HTMLROUTER = require("./app/routing/htmlRoutes.js");
-var APIROUTER = require("./app/routing/apiRoutes.js");
 
 //server configuration
 var app = express();
 // var PORT = 3000;
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({
-    extended: true
-}));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//new HTMLROUTER object from constructor in htmlRoutes.js
-var htmlRouter = new HTMLROUTER();
-htmlRouter.routeHTML();
+//import routers for html and api
+require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
 
 // Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
