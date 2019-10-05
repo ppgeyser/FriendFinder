0f9f4f638d6ed2ed68bc1d998075460e901b8e59
Peter Geyser <peterpgeyser@gmail.com>
2019-10-05 10:06:57 -0700

    Fixed issue that would cause the user to match with themselves if they took quiz multiple times

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,96 +5,96 @@ var friendArray = [
         "name": "Peter",
         "photo": "https://scontent.fsan1-1.fna.fbcdn.net/v/t1.0-9/50461253_10156093396766724_3358412796223029248_o.jpg?_nc_cat=111&_nc_oc=AQmoNHyQX-XOpEAy025mE8C18Su5swFH_CNPNsduNyWRB4hJXTl_1Nz3Cpm4d65M6FU&_nc_ht=scontent.fsan1-1.fna&oh=a88682b8d2dbc112882b73696127221c&oe=5DF9D7A1",
         "scores": [
-            1,
-            3,
-            1,
-            0,
-            4,
-            2,
-            3,
-            0,
-            0,
-            2
+            "1",
+            "3",
+            "1",
+            "0",
+            "4",
+            "2",
+            "3",
+            "0",
+            "0",
+            "2"
         ]
     },
     {
         "name": "Sarah",
         "photo": "https://images.unsplash.com/photo-1535324492437-d8dea70a38a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1287&q=80",
         "scores": [
-            4,
-            1,
-            1,
-            0,
-            1,
-            4,
-            0,
-            3,
-            0,
-            0
+            "4",
+            "1",
+            "1",
+            "0",
+            "1",
+            "4",
+            "0",
+            "3",
+            "0",
+            "0"
         ]
     },
     {
         "name": "Troy",
         "photo": "https://images.unsplash.com/photo-1556157382-97eda2d62296?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
         "scores": [
-            0,
-            0,
-            4,
-            3,
-            5,
-            1,
-            0,
-            0,
-            0,
-            5
+            "0",
+            "0",
+            "4",
+            "3",
+            "5",
+            "1",
+            "0",
+            "0",
+            "0",
+            "5"
         ]
     },
     {
         "name": "Liz",
         "photo": "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1300&q=80",
         "scores": [
-            3,
-            3,
-            0,
-            1,
-            0,
-            4,
-            3,
-            3,
-            4,
-            2
+            "3",
+            "3",
+            "0",
+            "1",
+            "0",
+            "4",
+            "3",
+            "3",
+            "4",
+            "2"
         ]
     },
     {
         "name": "Leif",
         "photo": "https://images.unsplash.com/photo-1526328398151-ad1065c82a0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
         "scores": [
-            0,
-            0,
-            0,
-            4,
-            5,
-            0,
-            5,
-            0,
-            0,
-            4
+            "0",
+            "0",
+            "0",
+            "4",
+            "5",
+            "0",
+            "5",
+            "0",
+            "0",
+            "4"
         ]
     },
     {
         "name": "Katie",
         "photo": "https://images.unsplash.com/photo-1543949806-2c9935e6aa78?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1234&q=80",
         "scores": [
-            2,
-            2,
-            0,
-            1,
-            1,
-            1,
-            0,
-            2,
-            0,
-            0
+            "2",
+            "2",
+            "0",
+            "1",
+            "1",
+            "1",
+            "0",
+            "2",
+            "0",
+            "0"
         ]
     }
 ];
